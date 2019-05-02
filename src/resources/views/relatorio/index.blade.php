339c66e09cd52b4f570eb87b3ef0a10fa4df592f
Administrador <mattos.conceicao@yahoo.com.br>
2019-05-02 02:11:08 -0300

    Adicionado relatorio.

---
+++ src/resources/views/relatorio/index.blade.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- As 3 meta tags acima *devem* vir em primeiro lugar dentro do `head`; qualquer outro conteúdo deve vir *após* essas tags -->
+    <title>Relatório</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+        .table td {
+            /*text-align: center;*/
+            padding: 0;
+        }
+
+        .table {
+            width: 90%;
+            margin: 0px auto;
+            float: none;
+        }
+
+        table, th, td {
+            border: 1px solid black;
+        }
+    </style>
+
+</head>
+<body>
+
+<table class="table" style="text-align: center; background-color: #c0c0c0;">
+    <tbody>
+    <tr>
+        <th>CONDER</th>
+        <th>Companhia de Desenvolvimento
+            Urbano do Estado da Bahia</th>
+        <th>BOLETIM DE REAJUSTAMENTO DA MEDIÇÃO
+            No 16</th>
+    </tr>
+    </tbody>
+</table>
+<br>
+<table class="table">
+    <tbody>
+    <tr>
+        <td>CONTRATADA:</td>
+        <td colspan="2">CONTRATO N°</td>
+        <td colspan="3">MEDIÇÃO A P. INICIAL:</td>
+        <td colspan="3">DATA BASE:</td>
+    </tr>
+    <tr>
+        <td>SERVIÇO:</td>
+        <td colspan="2">REAJUST. ACUMULADO:</td>
+        <td colspan="3">REAJUATAMENTO:</td>
+        <td colspan="3">CONCLUSÃO PREVISTA:</td>
+    </tr>
+    <tr>
+        <td>LOCAL:</td>
+        <td colspan="2">PERCENTUAL MÉDIO:</td>
+        <td colspan="3">PERCENTUAL MÉDIO:</td>
+        <td colspan="3">PERÍODO:</td>
+    </tr>
+    <tr style="background-color: #c0c0c0;">
+        <th rowspan="2">ITEM</th>
+        <th rowspan="2">DISCRIMINAÇÃO</th>
+        <th rowspan="2">UNID.</th>
+        <th colspan="3">INDICES D OBRAS RODOVIÁRIAS - FGV</th>
+        <th colspan="3">VALORES</th>
+    </tr>
+
+    <tr style="background-color: #c0c0c0;">
+        <th>INDICADOR</th>
+        <th>DEZ/2015</th>
+        <th>DEZ/2018</th>
+        <th>MEDIÇÃO</th>
+        <th>FATOR %</th>
+        <th>REAJUST.</th>
+    </tr>
+
+    <tr >
+        <td style="border: none">TOTAL</td>
+    </tr>
+
+    <tr >
+        <td style="border-top: black solid 1px">Obs:</td>
+        <td colspan="5" style="border-top: black solid 1px">Reajuste da medição</td>
+        <td colspan="3" style="border-top: black solid 1px">Estorno do reajuste N.15</td>
+    </tr>
+    </tbody>
+</table>
+
+</body>
+</html>
--- src/routes/web.php
+++ src/routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/relatorio', function () {
+    return view('relatorio.index');
+});
